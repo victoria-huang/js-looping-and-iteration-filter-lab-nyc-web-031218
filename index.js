47601f9fe43aaef21a358e6887d7fa4d7c49518a
victoria-huang <vctrah@gmail.com>
2018-04-22 20:53:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  const matchedArr = drivers.filter(
+    function (driver) {
+      return driver.toUpperCase() === string.toUpperCase();
+    }
+  );
+  
+  return matchedArr;
+}
+
+function fuzzyMatch(drivers, string) {
+  const matchedArr = drivers.filter(
+    function(driver) {
+      const upperCaseDriver = driver.toUpperCase();
+      const upperCaseString = string.toUpperCase();
+      return upperCaseDriver.startsWith(upperCaseString);
+    }
+  );
+
+  return matchedArr;
+}
+
+function matchName(drivers, string) {
+  const matchedArr = drivers.filter(
+    function(driver) {
+      const name = driver.name.toUpperCase();
+      return name === string.toUpperCase();
+    }
+  );
+
+  return matchedArr;
+}
